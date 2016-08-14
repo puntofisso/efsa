04d639d04d5eed59399344eca6b5c76fd5840baf
Giuseppe Sollazzo <puntofisso@gmail.com>
2016-08-14 19:00:11 +0200

    More endpoints

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -28,6 +28,47 @@ Route::get('/example', function () {
 } );
 
 Route::get('/questions', function() {
-	$users = DB::select('select * from efsa.Questions limit 10');
-	return json_encode($users);
+	$questions = DB::select('select * from efsa.Questions limit 10');
+	return json_encode($questions);
+});
+
+Route::get('/questions/{id}', function ($id) {
+
+
+    $questions = DB::select('select * from efsa.Questions where efsa.Questions.QUESTIONNUMBER = :id', ['id' => $id]);
+	return json_encode($questions);
+});
+
+Route::get('/questions/{id}/status', function ($id) {
+
+
+    $question = DB::select('select STATUS from efsa.Questions where efsa.Questions.QUESTIONNUMBER = :id', ['id' => $id]);
+
+    $valid_status = ['In progress','Under Consideration','Registration not yet completed','Additional data request','Finished','Waiting for full dossier','Not accepted','Withdrawn','Deleted'];
+
+    $out['question'] = $question;
+    $out['list_of_possible_status'] = $valid_status;
+
+	return json_encode($out);
+});
+
+Route::get('/unitpanel/all', function() {
+	$query=DB::select('SELECT distinct UNIT,PANEL FROM Questions ORDER BY `Questions`.`UNIT` ASC' );
+	return json_encode($query);
+});
+
+Route::get('/unitpanel/units', function() {
+	$query=DB::select('SELECT distinct UNIT FROM Questions ORDER BY `Questions`.`UNIT` ASC' );
+	return json_encode($query);
+});
+
+Route::get('/unitpanel/panels', function() {
+	$query=DB::select('SELECT distinct PANEL FROM Questions ORDER BY `Questions`.`PANEL` ASC' );
+	return json_encode($query);
+});
+
+Route::get('/petitioners', function() {
+	$query=DB::select('SELECT PETITIONER, COUNT(PETITIONER) AS COUNT FROM Questions GROUP BY PETITIONER ORDER BY PETITIONER');
+		return json_encode($query);
+
 });
